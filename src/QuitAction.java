33482b8b137cea8cad966b5022007864928682eb
Will-R-UC <rahtzwd@mail.uc.edu>
2026-02-11 19:08:45 -0500

    Feat: Create quit program functionality

---
+++ src/QuitAction.java
@@ -0,0 +1,8 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class QuitAction implements ActionListener {
+    public void actionPerformed(ActionEvent ae) {
+        System.exit(0);
+    }
+}
